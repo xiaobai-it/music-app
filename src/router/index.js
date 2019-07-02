780e507da7575108643ab5299e88e8f45a5cbf05
qwd_global <qwd_global@qq.com>
2019-07-02 19:17:32 +0800

    加入过渡页

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ import singer from '../pages/singer/singer' // 歌手页面
 import singerDetail from '../pages/singer-detail/singer-detail' // 歌手详情页面
 import recommendDetail from '../pages/recommend-detail/recommend-detail' // 推荐页面的详情页面
 import userCenter from '../pages/user-center/user-center' // 排行页面
+import loading from '../pages/loading/loading' // 进入的时候的加载页面
 
 Vue.use(Router)
 
@@ -58,8 +59,20 @@ export default new Router({
       path: '/user',
       component: userCenter
     },
+    // {
+    //   path: '/',
+    //   redirect: '/recommend'
+    // },
+    {
+      path: '/loading',
+      component: loading
+    },
     {
       path: '/',
+      redirect: '/loading'
+    },
+    {
+      path: '*',
       redirect: '/recommend'
     }
   ]
